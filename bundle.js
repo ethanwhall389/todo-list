656d883d9f7a9260c55621ee25496342235dfd30
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2023-10-11 21:14:49 +0000

    Begin adding js modules

--- bundle.js
+++ bundle.js
@@ -7,26 +7,100 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/javascript/index.js":
 /*!*********************************!*\
   !*** ./src/javascript/index.js ***!
   \*********************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log('test');\n\nconst myVar = 10;\n\n//# sourceURL=webpack://todo-list-rewritten/./src/javascript/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _ui_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ui.js */ \"./src/javascript/ui.js\");\n\n\ndocument.addEventListener('DOMContentLoaded', _ui_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"].loadPage())\n\n\n// functions\n// add new task\n// delete task\n\n\n// modules\n    // UI\n    // task-handling\n    // List\n\n//# sourceURL=webpack://todo-list-rewritten/./src/javascript/index.js?");
+
+/***/ }),
+
+/***/ "./src/javascript/list.js":
+/*!********************************!*\
+  !*** ./src/javascript/list.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ List)\n/* harmony export */ });\nclass List {\n    constructor() {\n        this.tasks = ['test01', 'test02', 'test03'];\n\n    }\n\n    static getTasks () {\n        return this.tasks;\n    }\n\n    addTask (task) {\n        this.tasks.push(task);\n    }\n\n    removeTask (indexToRemove) {\n        this.tasks.splice(indexToRemove, 1);\n    }\n    \n    setAllTasks (tasks) {\n        this.tasks = tasks; \n    }\n}\n\n//# sourceURL=webpack://todo-list-rewritten/./src/javascript/list.js?");
+
+/***/ }),
+
+/***/ "./src/javascript/ui.js":
+/*!******************************!*\
+  !*** ./src/javascript/ui.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ UI)\n/* harmony export */ });\n/* harmony import */ var _list_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./list.js */ \"./src/javascript/list.js\");\n\n\nclass UI {\n    static loadPage() {\n        UI.loadTasks();\n    }\n\n    static loadTasks () {\n        console.log('Tasks: ' + JSON.stringify(_list_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"].getTasks()))\n    }\n}\n\n\nconst taskInput = document.querySelector('#input-box');\nconst addTaskBttn = document.querySelector('#add-task-bttn');\n\n//# sourceURL=webpack://todo-list-rewritten/./src/javascript/ui.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/javascript/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/javascript/index.js");
 /******/ 	
 /******/ })()
 ;
